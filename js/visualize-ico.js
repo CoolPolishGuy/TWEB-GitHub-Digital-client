634bded03dc4124e88e3da0961ce23cb530ae34a
Grem25 <jeremie.zanone@heig-vd.ch>
2017-11-24 11:37:26 +0100

    modif index

--- js/visualize-ico.js
+++ js/visualize-ico.js
@@ -11,13 +11,13 @@
       .await(taskAreDone);
   }
   
-  var margin = { top: 20, right: 20, bottom: 50, left:150};
+  var margin = { top: 20, right: 20, bottom: 50, left:80};
   var width = 1500 - margin.left - margin.right;
   var height = 600 - margin.top - margin.bottom;
 
   const parseDate = d3.utcParse('%Y-%m-%d %H:%M:%S');
 
-  var svg = d3.select("body").append("svg")
+  var svg = d3.select("#icojs").append("svg")
   .attr('width', width + margin.left + margin.right)
   .attr('height', height + margin.top + margin.bottom)
   .append('g')
