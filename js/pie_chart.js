75764d6f2fab0a4eaa71a4d249b1a0b89217df9b
Grem25 <jeremie.zanone@heig-vd.ch>
2017-11-30 21:25:54 +0100

    avancement

--- js/pie_chart.js
+++ js/pie_chart.js
@@ -1,8 +1,30 @@
-
-
 var site_dropdown = d3.select("#pie").append("div")
  .attr("class", "site-dropdown")
  .append("select");
+
+var svg5 = d3.select("#pie").append("svg")
+ .attr("class", "pie-chart")
+ .attr('height',520)
+ .attr('width',600);
+ 
+var width5 = +svg5.attr("width");
+var height5 = +svg5.attr("height");
+var radius = Math.min(width5, height5) / 2;
+var g = svg5.append("g").attr("transform", "translate(" + width5 / 2 + "," + height5 / 2 + ")");
+
+var path = d3.arc()
+  .outerRadius(radius-30)
+  .innerRadius(75);
+
+var label = d3.arc()
+  .outerRadius(radius+20)
+  .innerRadius(radius+10);
+
+var pie = d3.pie()
+  .value(function(d) { return d.count; });  ;
+
+var  color1 = d3.scaleOrdinal(["#00ff99", "#ff3399"]);
+
 
 d3.csv("../data/moyenne_crypto_stock.csv", function(d) {
   return {
@@ -11,46 +33,17 @@ d3.csv("../data/moyenne_crypto_stock.csv", function(d) {
   };
 }, function(error, data) {
 
-site_dropdown.selectAll("option")
-  .data(data)
-  .enter()
-  .append("option")
-  .property("selected", function (d) { return d.date == '2015-08'; })
-  .attr("value", function (d) { return d.values; })
-  .text(function (d) { return d.date; });
+  site_dropdown.selectAll("option")
+    .data(data)
+    .enter()
+    .append("option")
+    .property("selected", function (d) { return d.date == '2015-08'; })
+    .attr("value", function (d) { return d.values; })
+    .text(function (d) { return d.date; });
 
 
 
-
-
-
-
-
-
-
-  var svg5 = d3.select("#pie").append("svg")
-  .attr("class", "pie-chart")
-  .attr('height',520)
-  .attr('width',520);
-  
-  width5 = +svg5.attr("width"),
-  height5 = +svg5.attr("height"),
-  radius = Math.min(width5, height5) / 2,
-  g = svg5.append("g").attr("transform", "translate(" + width5 / 2 + "," + height5 / 2 + ")");
-  
-  var pie = d3.pie()
-    .value(function(d) { return d.count; });  ;
-  
-  var path = d3.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(0);
-  
-  var label = d3.arc()
-    .outerRadius(radius - 60)
-    .innerRadius(radius - 100);
-  
-  
-    let color = d3.scaleOrdinal(["#00ff99", "#ff3399"]);
+    
  
     let res = [
       { label: 'crypto', count: parseFloat(data[0].values[0]) }, 
@@ -64,12 +57,46 @@ site_dropdown.selectAll("option")
   
     arc.append("path")
         .attr("d", path)
-        .attr("fill", function(d) { return color(d.data.count); });
-  
+        .attr("fill", function(d) { return color1(d.data.count); })
+        .transition().delay(function(d,i) {
+          return i * 500; })
+        .duration(500)
+        .attrTween('d', function(d) {
+            var i = d3.interpolate(d.startAngle+0.1, d.endAngle);
+            return function(t) {
+              d.endAngle = i(t); 
+              return path(d)
+              }
+        });
+
+    arc.each(function (d) { this._current = d; })
+    .on('mouseover', function (d, i) {
+      
+      d3.select(this)
+        .transition()
+          .duration(500)
+      .attr('transform', function (d) {
+        var dist = 15;
+        d.midAngle = ((d.endAngle - d.startAngle) / 2) + d.startAngle;
+        var x = Math.sin(d.midAngle) * dist;
+        var y = -Math.cos(d.midAngle) * dist;
+        return 'translate(' + x + ',' + y + ')';
+      });
+    })
+    .on('mouseout', function (d, i) {
+      d3.select(this)
+        .transition()
+          .duration(500)
+          .attr('transform', 'translate(0,0)')
+    }); 
+      
+       
     
     arc.append("text")
     .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
     .attr("dy", "0.35em")
+    .transition()
+    .delay(1000)
     .text(function(d) { return d.data.label; });  
 
 
@@ -78,42 +105,30 @@ site_dropdown.selectAll("option")
 
 
 
-d3.selectAll(".site-dropdown")
-    .selectAll("select")
-    .on("change", updatePieChart);
+  d3.selectAll(".site-dropdown")
+      .selectAll("select")
+      .on("change", updatePieChart);
 });
-
 
 
 
 function updatePieChart() {
 
 
-d3.selectAll(".pie-chart").remove()
-  
-var svg5 = d3.select("#pie").append("svg")
-.attr("class", "pie-chart")
-.attr('height',520)
-.attr('width',520);
+  d3.selectAll(".pie-chart").remove()
+    
+  svg5 = d3.select("#pie").append("svg")
+  .attr("class", "pie-chart")
+  .attr('height',520)
+  .attr('width',800);
 
-width5 = +svg5.attr("width"),
-height5 = +svg5.attr("height"),
-radius = Math.min(width5, height5) / 2,
-g = svg5.append("g").attr("transform", "translate(" + width5 / 2 + "," + height5 / 2 + ")");
-
-var pie = d3.pie()
-  .value(function(d) { return d.count; });  ;
-
-var path = d3.arc()
-  .outerRadius(radius - 10)
-  .innerRadius(0);
-
-var label = d3.arc()
-  .outerRadius(radius - 60)
-  .innerRadius(radius - 100);
+  width5 = +svg5.attr("width"),
+  height5 = +svg5.attr("height"),
+  radius = Math.min(width5, height5) / 2,
+  g = svg5.append("g").attr("transform", "translate(" + width5 / 2 + "," + height5 / 2 + ")");
 
 
-  let color = d3.scaleOrdinal(["#00ff99", "#ff3399"]);
+
   let repartition = this.options[this.selectedIndex].value;
   let crypto = repartition.substring(0,repartition.indexOf(','))
   let stock = repartition.substring(repartition.indexOf(',') + 1,repartition.length)
@@ -129,11 +144,12 @@ var label = d3.arc()
 
   arc.append("path")
       .attr("d", path)
-      .attr("fill", function(d) { return color(d.data.count); });
+      .attr("fill", function(d) { return color1(d.data.count); });
+      
 
   
   arc.append("text")
   .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
   .attr("dy", "0.35em")
   .text(function(d) { return d.data.label; });  
-  }
+}
